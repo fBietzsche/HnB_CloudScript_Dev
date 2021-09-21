4fdca99ab942c5eff770b155e09c2d50d92cea48
Mehmet Hayri Çakır <36895137+mhmtckr27@users.noreply.github.com>
2021-09-21 14:04:51 +0300

    try

--- CloudScript.js
+++ CloudScript.js
@@ -935,6 +935,14 @@ handlers.EndMatch = function (args) {
     let loserPlayers = !args.loserPlayers ? {} : args.loserPlayers;
     let drawPlayers = !args.drawPlayers ? {} : args.drawPlayers;
 
+    winnerPlayers =
+        [
+            {
+            "PlayfabID" : "F8DF7529B62ED448",
+            "kills" : 10,
+            "deaths" : 1
+            }
+        ];
     //Win
     for (i = 0; i < winnerPlayers.length; i++) {
         winnerPlayers[i].KDAScore = GetKDAScore(winnerPlayers[i]);
